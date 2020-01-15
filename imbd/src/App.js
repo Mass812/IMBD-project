10abd57e8832a80be51151e12d1318f25f83744e
mattwellman812 <williamwellman98@gmail.com>
2020-01-15 16:05:02 -0500

    quick build

---
+++ imbd/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieCard from './Movie/MovieCard'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <MovieCard />
+     </div>
+  );
+}
+
+export default App;
---
+++ imbd/src/Movie/MovieCard.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './MovieCard.scss';
+
+const MovieCard = () => {
+  const [searchTerm, setSearchTerm] = useState("Frozen");
+  const [typed, setTyped] = useState('');
+  const [moviesReturned, setMoviesReturned] = useState([{'movie': 'Frozen'}]);
+
+
+  useEffect(() => {
+    const movieDB = `https://api.themoviedb.org/3/search/movie?api_key=2121f2ad7169f32e4b2cab5cf77d32cd&query=${searchTerm}`;
+
+    axios
+      .get(movieDB)
+      .then(res => [res.data.results])
+      .then(res=> setMoviesReturned(res[0]))
+ 
+       
+      
+  
+      
+      .catch(err => console.error(err, "☹️"));
+      
+      
+      return () => {
+        console.log('moviesReturned with focus', );
+    };
+  }, [searchTerm]);
+
+  //const showDetails = moviesReturned.Search.map(movie=> movie.imbID);
+
+  const searchForTyped = () => {
+    setSearchTerm(typed);
+  };
+
+  const storeTypedFx = e => {
+    setTyped(e.target.value);
+  };
+
+console.log('after useEffect movies returned: ', moviesReturned)
+
+
+
+
+const displayMovies = moviesReturned.map((n,id)=> (
+
+<div key={id} className='movie-card-container'>
+<div className = 'image-container'>
+<img className='movie-card-image' src={`https://image.tmdb.org/t/p/w500${n.poster_path}`} alt={n.title}/>
+
+</div>
+    
+  <div className="movie-info">
+    <div className="movie-head-title">{n.title} </div>
+    <div className="movie-overview">{n.overview}</div>
+    <div className="movie-date">{n.release_date}</div>
+    <div className="movie-genre">{n.genre_ids}</div>
+    <div className="movie-head-3">{n.poster_path}</div>
+    <div className="movie-popularity">{n.popularity}</div>
+  </div>
+</div>
+
+
+
+  ))
+  console.log('one pull: ====> ', moviesReturned)
+
+  return (
+    <div className='movie-card--housing-container'>
+      <div className="movie-button--housing">
+      <h1> 
+      Movie Splat
+      </h1>
+        <input
+        className='search-box'
+          placeholder="Search for a Movie"
+          onChange={storeTypedFx}
+          onBlur={searchForTyped}
+        />
+        <button className='search-button' onSubmit={searchForTyped}>Search Movies </button>
+        
+    <div>{displayMovies}</div>
+   
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
+
+
+
+
+
