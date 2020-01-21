934010cbf626e3cc530951f01149f457f418901d
mattwellman812 <williamwellman98@gmail.com>
2020-01-21 12:34:21 -0500

    no message

--- src/Components/Framework/Framework.jsx
+++ src/Components/Framework/Framework.jsx
@@ -9,21 +9,23 @@ const Framework = () => {
   return (
     <div className='framework-container'>
       <header className='framework-header'>
+      <div className='framework-under-header' />
+        <div className='tmbd-issue'>
+          "I use the TMDb API, this site's not endorsed or certified by TMDb."
+        </div>
         <img
           className='greenLantern'
           src={greenLanternLogo}
           alt={'Matt Wellman Logo'}
         />
-        <h1>Movie Warehouse</h1>
-        <div className='tmbd-issue'>
-          "I use the TMDb API, this site's not endorsed or certified by TMDb."
-        </div>
+        <div className='title-top'>Movie</div> 
+        <div className='title-bottom'>Warehouse</div>
+      <div className='framework-under-header' />
       </header>
       <div className='framework'>
-      <div className='framework-under-header' />
         <section className='framework-section-one'>
           {' '}
-          New & Trending
+          New & Trending <br/>
           <LeftSection />
          
         </section>
