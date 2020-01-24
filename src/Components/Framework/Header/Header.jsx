e624f52006a43a03e27877fe7a85a44ee23cda34
mattwellman812 <williamwellman98@gmail.com>
2020-01-23 22:23:44 -0500

    no message

--- src/Components/Framework/Header/Header.jsx
+++ src/Components/Framework/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {  } from "@fortawesome/free-regular-svg-icons";
 const greenLanternLogo = require('../../../Assets/greenLanternMWlogo2.jpg');
 
 
@@ -15,6 +16,11 @@ const greenLanternLogo = require('../../../Assets/greenLanternMWlogo2.jpg');
         />
         <div className='title-top'>Movie</div>
         <div className='title-bottom'>Warehouse</div>
+        {/* <FontAwesomeIcon
+                    icon={faArrow}
+                    className="icon-bar-li-bio"
+                    size="lg"
+                  /> */}
         <div className='framework-under-header' />
       </header>
     )
